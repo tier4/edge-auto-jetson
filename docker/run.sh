840c5437be15528f7135163fda7da0e00ccd8ae7
Akihito Ohsato <aohsato@gmail.com>
2022-12-27 09:11:08 +0900

    feat: support gui on docker
    Signed-off-by: Akihito Ohsato <aohsato@gmail.com>

--- docker/run.sh
+++ docker/run.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
-source /opt/autoware/env/autoware.env
-
 SCRIPT_DIR=$(readlink -f "$(dirname "$0")")
 ROOT_DIR=${SCRIPT_DIR}/..
 
 COMMAND_IN_CONTAINER=${1:-/bin/bash}
 
+source /opt/autoware/env/autoware.env
+
+xhost +local:
+
 docker run -it --rm --net host --privileged \
            --gpus all \
-           -v /opt/autoware:/opt/autoware \
-           -v /sys:/sys \
            -e CYCLONEDDS_URI \
+           -e DISPLAY=$DISPLAY \
            -e RMW_IMPLEMENTATION \
            -e ROS_DOMAIN_ID \
+           -v /opt/autoware:/opt/autoware \
+           -v /sys:/sys \
+           -v /tmp/.X11-unix/:/tmp/.X11-unix: \
            -v ${ROOT_DIR}:/workspace \
            -w /workspace \
            perception_ecu:latest \
