94505504f31f5a3e9079f1e450f0f861cda4bde2
Akihito Ohsato <aohsato@gmail.com>
2022-12-28 14:18:38 +0900

    docs: fix typo
    Co-authored-by: Daisuke Nishimatsu <42202095+wep21@users.noreply.github.com>

--- docker/build.sh
+++ docker/build.sh
@@ -2,5 +2,5 @@
 
 SCRIPT_DIR=$(readlink -f "$(dirname "$0")")
 
-echo "Builing docker image based on Dockerfile"
+echo "Building docker image based on Dockerfile"
 docker build --platform arm64 -t perception_ecu -f ${SCRIPT_DIR}/Dockerfile ${SCRIPT_DIR}
