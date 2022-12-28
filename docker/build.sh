b753b8091afbe287eecc578fc25b82c54879efd2
Akihito OHSATO <aohsato@gmail.com>
2022-12-28 21:42:09 +0900

    style: fix shellcheck errors
    Signed-off-by: Akihito OHSATO <aohsato@gmail.com>

--- docker/build.sh
+++ docker/build.sh
@@ -3,4 +3,4 @@
 SCRIPT_DIR=$(readlink -f "$(dirname "$0")")
 
 echo "Building docker image based on Dockerfile"
-docker build --platform arm64 -t perception_ecu -f ${SCRIPT_DIR}/Dockerfile ${SCRIPT_DIR}
+docker build --platform arm64 -t perception_ecu -f "${SCRIPT_DIR}"/Dockerfile "${SCRIPT_DIR}"
--- docker/run.sh
+++ docker/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 SCRIPT_DIR=$(readlink -f "$(dirname "$0")")
-ROOT_DIR=${SCRIPT_DIR}/..
+ROOT_DIR="${SCRIPT_DIR}"/..
 
-COMMAND_IN_CONTAINER=${1:-/bin/bash}
+COMMAND_IN_CONTAINER="${1:-/bin/bash}"
 
 source /opt/autoware/env/autoware.env
 
@@ -11,13 +11,13 @@ xhost +local:
 docker run -it --rm --net host --privileged \
     --gpus all \
     -e CYCLONEDDS_URI \
-    -e DISPLAY=$DISPLAY \
+    -e DISPLAY="${DISPLAY}" \
     -e RMW_IMPLEMENTATION \
     -e ROS_DOMAIN_ID \
     -v /opt/autoware:/opt/autoware \
     -v /sys:/sys \
     -v /tmp/.X11-unix/:/tmp/.X11-unix: \
-    -v ${ROOT_DIR}:/workspace \
+    -v "${ROOT_DIR}":/workspace \
     -w /workspace \
     perception_ecu:latest \
-    ${COMMAND_IN_CONTAINER}
+    "${COMMAND_IN_CONTAINER}"
