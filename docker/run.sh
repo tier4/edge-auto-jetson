37e7c98f35fcbdaf50dfd58b033b3ab70907f977
Akihito Ohsato <aohsato@gmail.com>
2023-02-05 23:53:18 +0900

    change: enable x11 forwarding for launch via ssh (#8)
    Signed-off-by: Akihito Ohsato <aohsato@gmail.com>

--- docker/run.sh
+++ docker/run.sh
@@ -7,18 +7,18 @@ COMMAND_IN_CONTAINER="${1:-/bin/bash}"
 # shellcheck disable=SC1091
 source /opt/autoware/env/autoware.env
 
-xhost +local:
-
 docker run -it --rm --net host --privileged \
     --gpus all \
     -e CYCLONEDDS_URI \
     -e DISPLAY="${DISPLAY}" \
+    -e NO_AT_BRIDGE=1 \
     -e RMW_IMPLEMENTATION \
     -e ROS_DOMAIN_ID \
+    -v "${HOME}"/.Xauthority:/root/.Xauthority:rw \
+    -v "${ROOT_DIR}":/workspace \
     -v /opt/autoware:/opt/autoware \
     -v /sys:/sys \
     -v /tmp/.X11-unix/:/tmp/.X11-unix: \
-    -v "${ROOT_DIR}":/workspace \
     -w /workspace \
     perception_ecu:latest \
     /bin/bash -c "${COMMAND_IN_CONTAINER}"
