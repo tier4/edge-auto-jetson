7a396e7c720fa7f85c4b87c5956e35247962a61b
Shanpeng MA <shanpeng.ma@tier4.jp>
2025-04-15 13:54:23 +0900

    sync 0.40.0

--- build.sh
+++ build.sh
@@ -8,14 +8,6 @@ if [ -z "$perception_type" ]; then
     exit 1
 fi
 
-if [ "$perception_type" != "addon" ]; then
-    echo "Applying BSP36.4 upgrade patches..."
-    # TODO: This is a patch for BSP36.4 upgrade
-    sed -i '/strategy::convex_hull::graham_andrew.*strategy;/d; /convex_hull(ring, convex_ring, strategy);/c\  boost::geometry::convex_hull(ring, convex_ring);' src/autoware.universe/common/autoware_universe_utils/src/geometry/random_concave_polygon.cpp
-    sed -i 's|#include <boost/geometry/strategies/agnostic/hull_graham_andrew.hpp>|#include <boost/geometry/algorithms/detail/convex_hull/graham_andrew.hpp>|' src/autoware.universe/common/autoware_universe_utils/src/geometry/random_concave_polygon.cpp
-    sed -i '/strategy::convex_hull::graham_andrew.*strategy;/d; /convex_hull(.*strategy);/c\  boost::geometry::convex_hull(ring, convex_ring);' src/autoware_utils/autoware_utils/src/geometry/random_concave_polygon.cpp
-    sed -i 's|#include <boost/geometry/strategies/agnostic/hull_graham_andrew.hpp>|#include <boost/geometry/algorithms/detail/convex_hull/graham_andrew.hpp>|' src/autoware_utils/autoware_utils/src/geometry/random_concave_polygon.cpp
-fi
 
 if [ "$perception_type" = "addon" ]; then
     echo "Running rosdep update and install for addon..."
